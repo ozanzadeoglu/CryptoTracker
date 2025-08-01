52f740c9fbcbb07fed781a37b18549ff71a6fae7
ozanzadeoglu <ozanzadeoglu01@gmail.com>
2025-08-01 22:32:21 +0300

    Add HTTP-based internet connectivity verification to ConnectivityServiceImpl to adress a shortcoming of connectivity_plus package.
    -This verification adresses to case where user is connected to a wi-fi without internet connection.

--- lib/core/connectivity/impl/connectivity_service_impl.dart
+++ lib/core/connectivity/impl/connectivity_service_impl.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:http/http.dart' as http;
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:crypto_tracker/core/connectivity/i_connectivity_service.dart';
 
@@ -6,7 +7,7 @@ import 'package:crypto_tracker/core/connectivity/i_connectivity_service.dart';
 class ConnectivityServiceImpl implements IConnectivityService {
   final Connectivity _connectivity = Connectivity();
   final _controller = StreamController<bool>.broadcast();
-  
+
   bool _isOnline = true; // Assume online by default until the first check.
 
   @override
@@ -26,15 +27,40 @@ class ConnectivityServiceImpl implements IConnectivityService {
     _updateConnectionStatus(result);
   }
 
-  void _updateConnectionStatus(List<ConnectivityResult> result) {
-    // We consider the device "online" if it's connected to either WiFi or mobile data.
-    final hasConnection = result.contains(ConnectivityResult.mobile) || 
-                          result.contains(ConnectivityResult.wifi);
-    
+  void _updateConnectionStatus(List<ConnectivityResult> result) async {
+    // TODO: Check which conditions this method is fired, may need to listen AppLifeCycle
+    final hasConnection =
+        result.contains(ConnectivityResult.mobile) ||
+        result.contains(ConnectivityResult.wifi);
+
+    // If device is connected to mobile or wifi, it's double checked with _checkInternetConnectivity method.
+    final newIsOnline = hasConnection
+        ? await _checkInternetConnectivity()
+        : false;
+
     // Only emit a new value if the connectivity status has actually changed.
-    if (hasConnection != _isOnline) {
-      _isOnline = hasConnection;
+    if (newIsOnline != _isOnline) {
+      _isOnline = newIsOnline;
       _controller.add(_isOnline);
+    }
+  }
+
+  // Performs an actual internet connectivity check using HTTP request.
+  Future<bool> _checkInternetConnectivity() async {
+    try {
+      final response = await http
+          .get(Uri.parse("https://clients3.google.com/generate_204"))
+          .timeout(const Duration(seconds: 5));
+
+      // Check for the expected 204 status code and an empty body.
+      if (response.statusCode == 204 && response.body.isEmpty) {
+        return true;
+      } else {
+        return false;
+      }
+    } on Exception catch (_) {
+      // The request timed out.
+      return false;
     }
   }
 
